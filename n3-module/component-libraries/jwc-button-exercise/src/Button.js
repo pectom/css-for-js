f5280908802ceb5541b1c0948094a382b8fb1c5c
pecatoma <pecatoma@gmail.com>
2022-10-17 15:09:05 +0200

    Intialize project

---
+++ n3-module/component-libraries/jwc-button-exercise/src/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+import { COLORS } from "./constants";
+
+const Button = ({ variant, size, children }) => {
+  return "TODO";
+};
+
+export default Button;
